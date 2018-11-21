9c9d3d01d9298708c20901405314ad9133af3383
超哥1125 <fire_snow@163.com>
2018-11-21 18:08:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <sys-header/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script type="text/javascript">
+  import sysHeader from '@/components/header.vue'
+  export default {
+    components: {
+      sysHeader
+    }
+  }
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <h1>
+      <img src="../assets/logo.png" alt=""/> YNOTE
+    </h1>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+<style lang="less" scoped>
+  @height: 60px;
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: @height;
+    line-height: @height;
+    width: 100%;
+    background: #fff;
+    text-align: left;
+    border-bottom: 1px solid #ccc;
+    h1 {
+      height: 100%;
+      padding: 0;
+      margin: 0;
+      line-height: @height;
+      display: inline-block;
+    }
+    img {
+      height: 40%;
+      margin: 5px 0 0 10px;
+    }
+  }
+</style>
