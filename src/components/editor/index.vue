0de0197a975b228ed6bddff1996d2ce5799dffe2
超哥1125 <fire_snow@163.com>
2018-11-22 18:29:43 +0800

    update: style

---
+++ src/components/editor/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <iframe ref="editor" width="100%" height="100%" frameborder="0"></iframe>
+</template>
+<script type="text/javascript">
+export default {
+  name: 'editor',
+  data() {
+    return {
+      conValue: this.value
+    }
+  },
+  props: {
+    // 值
+    value: {
+      type: Object,
+      data() {
+        return {
+          style: {
+            fontSize: '14px'
+          },
+          content: ''
+        }
+      }
+    }
+  },
+  mounted() {
+    let $d = this.$refs.editor.contentWindow.document // IE、FF都兼容
+    $d.designMode = "on"
+    $d.contentEditable = true
+    $d.open()
+    $d.close()
+    $d.getElementsByTagName('body')[0].innerHTML = "<div>A</div><div>B</div><div>C</div>"
+  }
+}
+</script>
+<style lang="less" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <i class="iconfont icon-plus-circle"></i>
-    <vue-draggable-resizable class="note-item" v-for="(note, index) in notes" :key="index" :w="note.w || 200" :h="note.h || 300" :x="note.x || 200" :y="note.y || 0" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true">
+    <!-- 工具 -->
+    <div class="tools">
+      <i class="iconfont icon-plus-circle"></i>
+    </div>
+    <!-- 所有便签 -->
+    <vue-draggable-resizable :class="['note-item', note.theme]" v-for="(note, index) in notes" :key="index" :w="note.w || 200" :h="note.h || 300" :x="note.x || 200" :y="note.y || 0" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true">
       <p>
         {{note.content}}
       </p>
@@ -27,26 +31,37 @@ export default {
           h: 300,
           x: 200,
           y: 10,
-          content: `您好，欢迎使用YNOTE! `
+          content: `您好，欢迎使用YNOTE! `,
+          theme: ''
         },
-
         {
           w: 200,
           h: 300,
           x: 410,
           y: 50,
-          content: `点击“+”来创建你的笔记吧! `
+          content: `点击“+”来创建你的笔记吧! `,
+          theme: 'cfc'
+        },
+        {
+          w: 200,
+          h: 300,
+          x: 620,
+          y: 70,
+          content: `试试! `,
+          theme: 'ccf'
         }
       ]
     }
   },
   methods: {
+    // 便签拉动大小时
     onResize: function (x, y, width, height) {
       this.x = x
       this.y = y
       this.width = width
       this.height = height
     },
+    // 便签拖动时
     onDrag: function (x, y) {
       this.x = x
       this.y = y
@@ -58,10 +73,24 @@ export default {
   .home {
     position: relative;
     height: 100%;
+    .tools {
+      position: absolute;
+      right: 5px;
+      top: 5px;
+    }
     // 笔记面板样式
     .note-item {
       border: 1px solid #e8e4e4;
       box-shadow: 0px 2px 2px #bdc1ca;
+      background: #ffc;
+      // 淡绿
+      &.cfc {
+        background: #cfc;
+      }
+      // 淡紫
+      &.ccf {
+        background: #ccf;
+      }
     }
   }
 </style>
